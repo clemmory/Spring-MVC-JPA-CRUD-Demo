d9723dcbbbbc2b543155412d299422aa7b6246ec
clemmory <clementine.mory@gmail.com>
2024-01-31 12:23:01 +0100

    Estado Initial

---
+++ src/main/java/com/example/services/EmpleadoServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.EmpleadoDao;
+import com.example.entities.Empleado;
+
+@Service
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+    @Autowired
+    private EmpleadoDao empleadoDao;
+
+    @Override
+    public List<Empleado> dameTodosLosEmpleados() {
+        return empleadoDao.findAll();
+   }
+
+    @Override
+    public Empleado dameUnEmpleado(int idEmpleado) {
+        return empleadoDao.findById(idEmpleado).get();
+    }
+
+    @Override
+    public void eliminarEmpleado(int idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleado'");
+    }
+
+    @Override
+    public void persistirEmpleado(Empleado empleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'persistirEmpleado'");
+    }
+
+    @Override
+    public void actualizarEmpleado(Empleado empleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actualizarEmpleado'");
+    }
+
+}
